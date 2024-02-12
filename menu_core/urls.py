836cb7bdf237969f317f08640b729af39f9eb399
xzenon <xenon.nemrod@gmail.com>
2024-02-12 21:35:06 +0100

    Add urls

--- menu_core/urls.py
+++ menu_core/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from menu.routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include((router.urls, "menu_api"), namespace="menu_api"), name="menu_api"),
 ]
