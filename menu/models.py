e4234a997e04e3dae40a680870aa4028a6653f9a
xzenon <xenon.nemrod@gmail.com>
2024-02-12 21:34:24 +0100

    Add models

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        """
+        Return a string representation of the object.
+        """
+        return self.name
