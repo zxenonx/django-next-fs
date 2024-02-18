064dc90d7684e6bc2e79d5c7d7f8045fa949550c
xzenon <xenon.nemrod@gmail.com>
2024-02-18 22:55:39 +0100

    Add configs for django-cors-headers

--- menu_core/settings.py
+++ menu_core/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
 
     "rest_framework",
 
+    "corsheaders",
+
     "menu",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+]
